69d7673bb7d432e71050397e7446eef7a86cdd46
IvanKan <ivankan0408@gmail.com>
2025-08-06 16:48:40 +0800

    Use product.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,31 +1,7 @@
 // Step 1: save the data in JS code
-// Make sure it has the same structure 
-const products = [{
-    image: 'images/products/athletic-cotton-socks-6-pairs.jpg', 
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs', 
-    rating: {
-        stars: 4.5, 
-        count: 87
-    }, 
-    priceCents: 1090    // cuz js have some problem dealing with decimal
-}, {
-    image: 'images/products/intermediate-composite-basketball.jpg',
-    name: 'Intermediate Size Basketball',
-    rating: {
-        stars: 4,
-        count: 127
-    },
-    priceCents: 2095   
-}, {
-    image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating: {
-        stars: 4.5,
-        count: 56
-    },
-    priceCents: 799
-}]; 
+// Make sure it has the same structure  
 
+// Step 2: generate the HTML
 let productHTML = ''; 
 
 products.forEach((product) => {
