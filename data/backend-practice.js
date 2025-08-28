ebb41d348435fe0e223c9fa0276e893db9fd1737
IvanKan <ivankan0408@gmail.com>
2025-08-28 19:01:49 +0800

    Add backend practice file

---
+++ data/backend-practice.js
@@ -0,0 +1,21 @@
+/*
+To send message to the backend side, we will use XMLHttpRequest: a built-in class of JS 
+*/ 
+
+const xhr = new XMLHttpRequest();  // This creates a new HTTP message to send to the backend 
+
+xhr.addEventListener('load', () => {         // to wait after we receive the response 
+    console.log(xhr.response); 
+});
+
+xhr.open('GET', 'https://supersimplebackend.dev');   
+/*
+1st: type of HTTP message
+- 'GET': get some info from the backend 
+- 'POST'
+- 'PUT'
+- 'DELETE'
+
+2nd: where to send this HTTP message (url)
+*/
+xhr.send();
