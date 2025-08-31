c704ad757a58d548788f1d86c57d397cc498c0fa
IvanKan <ivankan0408@gmail.com>
2025-08-31 13:42:55 +0800

    Convert from promises to async await

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -5,6 +5,25 @@ import { loadCart } from "../data/cart.js";
 // import '../data/cart-class.js'; 
 // import '../data/backend-practice.js'
 
+async function loadPage() {    // async = makes a function that returns a promise, ONLY works with promise 
+    await loadProductsFetch();   // await => let us write asynchronous code like normal code 
+    // wait for ths line of code to finish first 
+    // **** We can ONLY use await when we are inside a async function ****
+
+    await new Promise((resolve) => {
+        loadCart(() => {             
+            resolve('value2');       // we can save this value with a variable, no need to use then() 
+        });
+    })
+
+    renderOrderSummary();           
+    renderPaymentSummary();
+
+    //return 'value2'           // this return value will be saved in the parameter of then() 
+}
+loadPage(); 
+
+/*
 Promise.all([      // this allows to run multiple promises at the same time 
     loadProductsFetch(),
     new Promise((resolve) => {
@@ -18,6 +37,7 @@ Promise.all([      // this allows to run multiple promises at the same time
     renderOrderSummary();           // 3rd step 
     renderPaymentSummary();
 });
+*/
 
 // resolve is a function, similar to done() 
 // let us control when to go to the next step
